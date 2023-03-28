43bc051268a8ae53b8d46f1e558dfa5648dee9b9
Dylan Prole <126928374+dylanprole@users.noreply.github.com>
2023-03-29 11:24:21 +1300

    Update simple_multiplication.py

--- 8_kyu/simple_multiplication.py
+++ 8_kyu/simple_multiplication.py
@@ -1,3 +1,6 @@
+# DESCRIPTION:
+# This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
 def simple_multiplication(number) :
     # Your code goes here
     if number % 2 == 0:
