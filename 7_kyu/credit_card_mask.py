5b1b76bb633e3967292f3eb6f54ee0a59dc46a96
Dylan Prole <126928374+dylanprole@users.noreply.github.com>
2023-03-29 11:21:37 +1300

    Create credit_card_mask.py

---
+++ 7_kyu/credit_card_mask.py
@@ -0,0 +1,16 @@
+# return masked string
+def maskify(cc):
+    # If string length is 4 or less
+    # just return the string
+    if len(cc) <= 4:
+        return cc
+    
+    # Create mask of length of
+    # string minus last 4 chars
+    mask = '#'*(len(cc)-4)
+    
+    # Add final 4 chars of string
+    masked_cc = mask + cc[-4:]
+    
+    # Return masked string
+    return masked_cc
